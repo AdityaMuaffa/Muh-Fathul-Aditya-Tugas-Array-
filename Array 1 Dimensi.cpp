b4cededcd127963039894ab2962f3951455829dc
AdityaMuaffa <97894550+AdityaMuaffa@users.noreply.github.com>
2022-02-25 18:18:42 +0700

    Add files via upload

---
+++ Array 1 Dimensi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int angka[]={13,23,98,106};
+	int a;
+	for (a=0;a<4;a++){
+		cout <<"Tahun ke-"<<a+1<<" Memiliki Koleksi = "<<angka[a]<<" Benda "<<endl;
+	}
+	return 0;
+}
